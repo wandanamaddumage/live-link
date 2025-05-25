a159f23c4a22b90cf3ce66c7c0aec4c35ece48d6
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-25 09:08:58 +0530

    feat: update env variable

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,10 @@
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
-import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
+import ChatHeader from "./ChatHeader";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
